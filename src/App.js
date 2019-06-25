cbcb80f363eb169481a0fd0b39efd72420bb5c11
Kingsley Silas <kinsomicrote.inc@gmail.com>
2019-06-25 09:02:57 +0100

    worked on the hook example

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import React from 'react';
 import './App.css';
-import Async from 'react-async';
+import { useAsync } from 'react-async';
 
-const loadJson = () =>
+const loadJson = async () =>
   fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => (res.ok ? res : Promise.reject(res)))
     .then(res => res.json())
 
 function App() {
+  const { data, error, isLoading } = useAsync({ promiseFn: loadJson })
+
+  if (isLoading) return "Loading..."
+  if (error) return `Something went wrong: ${error.message}`
+  if (data)
   return (
     <div className="container">
-      <Async promiseFn={loadJson}>
-        {({ data, err, isLoading }) => {
-          if (isLoading) return "Loading..."
-          if (err) return `Something went wrong: ${err.message}`
-
-          if (data)
-            return (
-              <div>
-                <div>
-                  <h2>React Async - Random Users</h2>
-                </div>
-                {data.map(user=> (
-                  <div key={user.username} className="row">
-                    <div className="col-md-12">
-                      <p>{user.name}</p>
-                      <p>{user.email}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            )
-        }}
-      </Async>
+      <div>
+        <h2>React Async - Random Users</h2>
+      </div>
+      {data.map(user=> (
+        <div key={user.username} className="row">
+          <div className="col-md-12">
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
